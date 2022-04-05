f079872bc6bbfee893beab4a950502fb292bac74
yunyshs01 <jeoung09@naver.com>
2022-04-05 14:45:46 +0900

    20220403 pypy해야되네 내시간 돌려줘~

---
+++ src/14442.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+#include<queue>
+#include<memory.h>
+using namespace std;
+
+int arr[1001][1001];
+int visit[1001][1001];
+
+struct dat
+{
+    int y;
+    int x;
+    int b;
+    int t;
+};
+
+int dy[] = {-1,0,1,0};
+int dx[] = {0,1,0,-1};
+
+
+int main(){
+    
+    int n, m, k;
+    memset(visit,-1,sizeof(visit));
+    scanf("%d %d %d",&n,&m,&k);
+    for(int i=0;i<n;i++){
+        char str[1002];
+        scanf(" %s",str);
+        for (int j=0;j<m;j++){
+            arr[i][j] = str[j] - '0';
+        }    
+    }
+    
+    queue<dat> q;
+    q.push({0,0,0,1});
+    visit[0][0] = 0;
+    int ans = -1;
+    while(!q.empty()){
+        dat now = q.front();
+        q.pop();
+
+        if (now.y == n-1 && now.x == m-1){
+            ans = now.t;
+            break;
+        }
+        
+        for(int d=0;d<4;d++){
+            int ny = now.y + dy[d];
+            int nx = now.x + dx[d];
+            if (ny>=n || ny<0 || nx<0 || nx >=m)continue;
+            if (arr[ny][nx] == 1 && now.b<k && (visit[ny][nx] == -1 || visit[ny][nx] > now.b+1) ){
+                visit[ny][nx] = now.b+1;
+                q.push({ny,nx,now.b+1,now.t+1});
+
+            }
+            else if (arr[ny][nx] == 0 && (visit[ny][nx] == -1 || visit[ny][nx] > now.b)){
+                visit[ny][nx] = now.b;
+                q.push({ny,nx,now.b,now.t+1});
+            }
+        }
+    }
+    printf("%d\n", ans);
+}
---
+++ src/14442.py
@@ -0,0 +1,41 @@
+"""
+Solving
+20200405
+14442.py
+벽 부수고 이동하기 2
+"""
+
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+n, m, k = map(int,input().split())
+li = []
+for _ in range(n):
+    li.append(list(map(int,input().strip())))
+
+
+q = deque([])
+q.append((0,0,0,0)) # y, x, broken walls, path length
+visit = [[-1]*m for _ in range(n)]
+visit[0][0] = 0
+ans = -1
+while q:
+    y,x,b,t = q[0]
+    q.popleft()
+    if y == n-1 and x == m-1:
+        ans = t+1
+        break
+
+    for dy,dx in zip((-1,1,0,0),(0,0,-1,1)):
+        ny = y + dy
+        nx = x + dx
+        if ny<0 or ny>=n or nx<0 or nx>=m:continue
+        if li[ny][nx] == 1 and b <k and (visit[ny][nx]==-1 or visit[ny][nx] > b+1) :
+            visit[ny][nx] = b+1
+            q.append((ny,nx,b+1,t+1))
+        elif li[ny][nx] == 0 and (visit[ny][nx]==-1 or visit[ny][nx] > b):
+            visit[ny][nx] = b
+            q.append((ny,nx,b,t+1))
+print(ans)
+
